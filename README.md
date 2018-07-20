a797fd81fbe238fb1c4e20dd78f663bdc7f7e5b8
inkspot <Wyattsimpson777@gmail.com>
2018-07-19 21:16:27 -0400

    final touches

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# LIRI
+# LIRI Bot
+
+## What is this?
+
+This LIRI bot is a node command line application that takes in a few pre-determined arguments and returns data specific to the search. A log of commands entered is kept for reference by one of the LIRI functions
+
+## How does it work?
+
+LIRI takes in 4 preset commands:
+* my-tweets
+    * Pulls the 20 most recent tweets from the user's Twitter (assuming they have the proper keys)
+* spotify-this-song
+    * Searches Spotify with the keyword(s) passed in the command line and returns information about the song
+* movie-this
+    * Searches OMDB with the keyword(s) passed in the command line and returns information about the movie or show
+* do-what-it-says
+    * Perfect for the indecisive! Picks a random command that was run previously and runs it again.
+
+If you can't remember these, don't worry! Running LIRI with no command will list all accepted input
--- liri.js
+++ liri.js
@@ -11,17 +11,25 @@ var client = new Twitter(keys.twitter);
 var cmnd = process.argv[2];
 var input = process.argv.slice(3).join(" ");
 
-if (cmnd === "my-tweets") {
-    myTweets();
-    console.log("my-tweets")
-} else if (cmnd === "spotify-this-song") {
-    spotifyThisSong();
-    console.log("spotify-this-song")
-} else if (cmnd === "movie-this") {
-    movieThis();
-    console.log("movie-this")
-} else if (cmnd === "do-what-it-says") {
-    console.log("do-what-it-says")
+switch (cmnd) {
+    case "my-tweets":
+        myTweets();
+        logCommand();        
+        break;
+    case "spotify-this-song":
+        spotifyThisSong();
+        logCommand();
+        break;
+    case "movie-this":
+        movieThis();
+        logCommand();
+        break;
+    case "do-what-it-says":
+        doWhatItSays();
+        // logCommand(); // I commented this out because i'm afraid of an infinite loop. Highly unlikely, but if you ran do-what-it-says, and it picked do-what-it-says, and then it picked do-what-it-says... You see where i'm going with this?
+        break;
+    default:
+        console.log("\nPlease enter a valid command\n   • my-tweets\n   • spotify-this-song\n   • movie-this\n   • do-what-it-says")
 }
 
 function myTweets() {
@@ -42,31 +50,84 @@ function myTweets() {
 
 function spotifyThisSong() {
     if (!input) {
-        input = "Clock Catcher"
+        input = "Clock Catcher" // I prefer this song to Ace of Bass, so I used it instead
     };
     spotify.search({ type: 'track', query: input, limit: 1 }, function(err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
-      var results = data.tracks.items;
-      var artistsObj = results[0].artists;
-      var artistsArr = "";
-      for (i = 0; i < artistsObj.length; i++) {
+        var results = data.tracks.items;
+        var artistsObj = results[0].artists;
+        var artistsArr = "";
+        for (i = 0; i < artistsObj.length; i++) {
         if (i === artistsObj.length - 1) {
             artistsArr = artistsArr + results[0].artists[i].name
         } else {
             artistsArr = artistsArr + results[0].artists[i].name + ", "
         }
-      };
-      console.log(
-          "\nTitle: " + results[0].name +
-          "\nArtists: " + artistsArr +
-          "\nAlbum: " + results[0].album.name +
-          "\nLink: " + results[0].external_urls.spotify + "\n"
+        };
+        console.log(
+            "\nTitle: " + results[0].name +
+            "\nArtists: " + artistsArr +
+            "\nAlbum: " + results[0].album.name +
+            "\nLink: " + results[0].external_urls.spotify + "\n"
       );
     });
 }
 
 function movieThis() {
-    
+    if (!input) {
+        input = "Mr. Nobody"
+    }
+    request("http://www.omdbapi.com/?apikey=af8e132c&t=" + input, function (error, response, body) {
+        if (error) {
+            return console.log("Error occured: ", error);
+        }
+        var results = JSON.parse(body);
+        console.log(
+            "\nTitle: " + results.Title +
+            "\nYear of Release: " + results.Year +
+            "\nIMDB Rating: " + results.Ratings[0].Value +
+            // "\nRotten Tomatoes Rating: " + results.Ratings[1].Value +
+            "\nCountry of Production: " + results.Country +
+            "\nLanguage(s): " + results.Language +
+            "\nActors: " + results.Actors +
+            "\nPlot Summary: " + results.Plot + "\n"
+        )
+});
+}
+
+function doWhatItSays() {
+    var arr = [];
+    fs.readFile('random.txt', "utf8", function(err, data) {
+        if (err) {
+            return console.log(err)
+        };
+        var txt = data.split(',');
+        var randNum = Math.floor(Math.random() * txt.length);
+        var doThis = txt[randNum];
+        var whatever = doThis.split(" ");
+        console.log("\nCan't think of a search? Hmm, how about this one?\n\n" + doThis + "\n");
+        cmnd = whatever[0];
+        input = whatever.slice(1).join(" ");
+        switch (cmnd) {
+            case "my-tweets":
+                myTweets();
+                break;
+            case "spotify-this-song":
+                spotifyThisSong();
+                break;
+            case "movie-this":
+                movieThis();
+                break;
+            // case "do-what-it-says": // Commented out because these arent being logged to the random.txt
+            //     doWhatItSays();
+        }
+    });
+}
+
+function logCommand() { // originally, i had it append to the file with a space after the comma, but that started making problems for the do-what-it-says-function
+    fs.appendFile('random.txt',  "," + cmnd + " " + input, "utf8", function() {
+        console.log("Logged!")
+    });
 }
